41b40d73daf2b56a33a2363e26563491188c19b0
ivicavukosavljevic <ivicavuk1212@gmail.com>
2022-12-14 18:25:55 +0100

    working on a NoteForm

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { NewNote } from "./NewNote";
 
-function App() {
-  return <h1>Hi Ivica</h1>
+export type Note = {
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
 }
 
-export default App
+export type Tag = {
+  id: string
+  label: string
+}
+
+function App() {
+  return (
+    <Container className="my-4">
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<NewNote />} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+    </Container>
+  );
+}
+
+export default App;
