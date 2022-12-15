b57e0f27f04a3e923d571d21f9f1806aaa8e4183
ivicavukosavljevic <ivicavuk1212@gmail.com>
2022-12-15 12:22:06 +0100

    New note creation

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,38 +1,85 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
+import { NoteList } from "./NoteList";
 
 export type Note = {
-  id: string
-} & NoteData
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
 
 export type NoteData = {
-  title: string
-  markdown: string
-  tags: Tag[]
-}
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
 
 export type Tag = {
-  id: string
-  label: string
-}
+  id: string;
+  label: string;
+};
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
+
   return (
     <Container className="my-4">
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/new" element={<NewNote />} />
-      <Route path="/:id">
-        <Route index element={<h1>Show</h1>} />
-        <Route path="edit" element={<h1>Edit</h1>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<NoteList />} />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </Container>
   );
 }
---
+++ vite-project/src/NoteList.tsx
@@ -0,0 +1,40 @@
+import {
+    Badge,
+    Button,
+    Card,
+    Col,
+    Form,
+    Modal,
+    Row,
+    Stack,
+  } from "react-bootstrap"
+import { Link } from "react-router-dom";
+
+export function NoteList() {
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Notes</h1>
+        </Col>
+        <Col xs="auto">
+            <Stack gap={2} direction="horizontal">
+                <Link to="/new">
+                    <Button variant="primary">Create</Button>
+                </Link>
+                <Button variant="outline-secondary">Edit Tags</Button>
+            </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className="mb-4">
+            <Col>
+            <Form.Group controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" />
+                </Form.Group></Col>
+        </Row>
+      </Form>
+    </>
+  );
+}
